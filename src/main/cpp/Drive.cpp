ddbfec454e976109c44ad0b36c0c87b4f6388cba
Tech Vikings <frctechvikings@gmail.com>
2023-01-30 11:28:33 -0400

    Estruturando Drive.h

---
+++ src/main/cpp/Drive.cpp
@@ -0,0 +1,15 @@
+#include "Drive.h"
+
+void Drivetrain::DriveLog(){}
+
+void Drivetrain::Drive(double yAxis, double zAxis, bool lock){}
+
+void Drivetrain::DriveSpeed(int proportion, double gain){}
+
+void Drivetrain::DriveSpeedMax(){}
+
+void Drivetrain::DriveSpeedStandard(){}
+
+void Drivetrain::DriveDirection(){}
+
+void Drivetrain::DriveInit(){}
