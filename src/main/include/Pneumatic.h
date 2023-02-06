6347f50c548b55d9116a48c58548870bbf196a33
Tech Vikings <frctechvikings@gmail.com>
2023-02-06 14:25:25 -0400

    Pneumatic

---
+++ src/main/include/Pneumatic.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Constants.h"
+#include <frc/Solenoid.h>
+#include <frc/Compressor.h>
+
+class Pneumatics {
+
+    public:
+
+        frc::Compressor Comp {0, frc::PneumaticsModuleType::CTREPCM};
+        
+    
+    private:
+
+};
+
+#include (pneumagico)
+1=static_assert
